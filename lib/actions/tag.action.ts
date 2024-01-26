b8f753e8d4a005d15ed6a1623ee76d657914f692
Marian Dobre <dobremrn12@gmail.com>
2024-01-26 14:39:19 +0200

    edited tag.action file

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -64,7 +64,7 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
   try {
     connectToDatabase();
 
-    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+    const { tagId, searchQuery } = params;
 
     const tagFilter: FilterQuery<ITag> = { _id: tagId };
 
